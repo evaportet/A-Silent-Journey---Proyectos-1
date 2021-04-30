2074963da155122bd4450cf2aa431ba8f3d5a611
Marc Calvet <marc.calvet@enti.cat>
2021-04-30 17:22:12 +0200

    Enemy movement and animation

---
+++ Assets/Scripts/log.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class log : Enemy
+{
+    public Transform target;
+    public float chaseRadius;
+    public float atackRadius;
+    public Transform homePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position,transform.position)<= chaseRadius && Vector3.Distance(target.position, transform.position)>atackRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime); 
+        }
+    }
+}
