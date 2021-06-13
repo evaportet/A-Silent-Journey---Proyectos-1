d47aadbd1b80d44af2439a8461013575b6aa0d85
Marc Calvet <marc.calvet@enti.cat>
2021-06-14 00:19:21 +0200

    h

--- Assets/Scripts/Objects/TreasureChest.cs
+++ Assets/Scripts/Objects/TreasureChest.cs
@@ -1,18 +1,27 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TreasureChest : Interactable
 {
     // Start is called before the first frame update
-    void Start()
+    public void OnDisable()
     {
         
+    }
+    public void OnEnable()
+    {
+        isOpen = false;
+    }
+    private void Start()
+    {
+        isOpen = IntToBool(PlayerPrefs.GetInt("open"));
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         
     }
 }
