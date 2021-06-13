2a4f8b08fa40991f7caa90c535775d348f602894
Marc Calvet <marc.calvet@enti.cat>
2021-06-13 17:15:06 +0200

    Cofre de la llave hecho pero no se guarda al cambiar de escenas

---
+++ Assets/Scripts/ScriptableObjects/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Inventory : ScriptableObject
+{
+    public item currentItem;
+    public List<item> items = new List<item>();
+    public int numberOfKeys;
+
+    public void AddItem(item itemToAdd)
+    {
+        if (itemToAdd.isKey)
+        {
+            numberOfKeys++;
+        }
+        else
+        {
+            if (!items.Contains(itemToAdd))
+            {
+                items.Add(itemToAdd);
+            }
+        }
+    }
+}
