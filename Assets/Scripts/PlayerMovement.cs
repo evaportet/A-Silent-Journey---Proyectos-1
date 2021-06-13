1aca94de9ef39261c4ef3e6ee5bdb283e23ffad5
Marc Calvet <marc.calvet@enti.cat>
2021-06-14 00:21:10 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/main' into main""
    This reverts commit 6a2c0b70d954f34099b17f7bc7bbe23efbc783c7.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,6 +22,8 @@ public class PlayerMovement : MonoBehaviour
     public FloatValue currentHealth;
     public SignalSender playerHealthSignal;
     public VectorValue startingPosition;
+    public Inventory playerInventory;
+    public SpriteRenderer receivedItemSprite;
     //public FloatValue initialHealth;
 
     //muerte
@@ -43,6 +45,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Is the player in an inteacticon
+        if(currentState == PlayerState.interact)
+        {
+            return;
+        }
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
@@ -68,7 +75,32 @@ public class PlayerMovement : MonoBehaviour
         yield return null;
         animator.SetBool("pattacking", false);
         yield return new WaitForSeconds(.6f);
-        currentState = PlayerState.walk;
+        if(currentState != PlayerState.interact)
+        {
+            currentState = PlayerState.walk;
+        }
+        
+    }
+    public void RaiseItem()
+    {
+        if(playerInventory.currentItem != null)
+        {
+            if (currentState != PlayerState.interact)
+            {
+                animator.SetBool("Receive item", true);
+                currentState = PlayerState.interact;
+                receivedItemSprite.sprite = playerInventory.currentItem.itemSprite;
+            }
+            else
+            {
+                animator.SetBool("Receive item", false);
+                currentState = PlayerState.idle;
+                receivedItemSprite.sprite = null;
+                playerInventory.currentItem = null;
+            }
+        }
+       
+        
     }
     void UpdateAnimationAndMove ()
     {
