6a2c0b70d954f34099b17f7bc7bbe23efbc783c7
Marc Calvet <marc.calvet@enti.cat>
2021-06-14 00:17:32 +0200

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 63d5edd139453128cd360c521f622d559bea32d5.

--- Assets/Scripts/Objects/TreasureChest.cs
+++ Assets/Scripts/Objects/TreasureChest.cs
@@ -1,111 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TreasureChest : Interactable
 {
-    public item contents;
-    public Inventory playerInventory;
-    public bool isOpen;
-    public SignalSender raiseItem;
-    public GameObject dialogBox;
-    public Text dialogText;
-    private Animator anim;
-
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B) && playerInRange)
-        {
-            
-            if (!isOpen)
-            {
-                if (dialogBox.activeInHierarchy)
-                {
-                    //if(Target.position.x == -13.37F)
-                    dialogBox.SetActive(false);
-                }
-                else
-                {
-                    // Dialog window on
-                    dialogBox.SetActive(true);
-                }
-                OpenChest();
-                
-            }
-            else
-            {
-                dialogBox.SetActive(false);
-                ChestAlreadyOpen();
-            }
-            
-
-        }
-        else if (!playerInRange)
-        {
-            if (dialogBox.activeInHierarchy)
-            {
-                //if(Target.position.x == -13.37F)
-                dialogBox.SetActive(false);
-            }
-        }
-    }
-    public void OpenChest()
-    {
-        // set chest to open       
-        anim.SetBool("opened", true);
-        isOpen = true;
-        // dialog text = contents text
-        dialogText.text = contents.itemDescription;
-        // add contents to the inventory
-        playerInventory.AddItem(contents);
-        playerInventory.currentItem = contents;
-        //Raise the signal to the player to animate
-        raiseItem.Raise();        
-        // raise the context clue
-        contextOn.Raise();
         
-    }
-
-    public void ChestAlreadyOpen()
-    {
-        //raise the signal to the player to stop animating
-        raiseItem.Raise();
-        //Dialog off
-        dialogBox.SetActive(false);
-           
-            
-            
-        
-       
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player")  && !isOpen)
-        {
-            playerInRange = true;
-            contextOn.Raise();
-
-        }
-    }
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player")  && isOpen)
-        {
-            playerInRange = false;
-            //dialogBox1.SetActive(false);
-
-            //dialogText.text = dialog;                    
-            contextOff.Raise();
-
-            //dialogBox.SetActive(false);
-        }
     }
 }
